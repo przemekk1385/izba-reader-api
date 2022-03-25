25f956f804494f97a52c297a0dfdd1627b039731
Przemysław Kaliś <przemek.kalis@gmail.com>
2022-03-25 22:45:26 +0100

    HTML services (#2)
    * Add html stuff
    
    * Add pyppeteer
    
    * Add response model
    
    * Use function-based services
    
    * Use context managers for getting content and browser

--- izba_reader/main.py
+++ izba_reader/main.py
@@ -4,8 +4,8 @@ from datetime import datetime
 from fastapi import FastAPI
 
 from izba_reader import timezones
-from izba_reader.models import RssFeedsResponse
-from izba_reader.tasks import fetch_rss_task
+from izba_reader.models import RssFeedsResponse, WebScrapersResponse
+from izba_reader.tasks import fetch_rss_feeds, scrap_web
 
 app = FastAPI()
 
@@ -17,10 +17,20 @@ def hello_world():
 
 @app.get("/rss_feeds", response_model=RssFeedsResponse)
 async def rss_feeds():
-    feeds = list(itertools.chain(*await fetch_rss_task()))
+    feeds = list(itertools.chain(*await fetch_rss_feeds()))
 
     return {
         "fetched_at": datetime.now().astimezone(timezones.EUROPE_WARSAW),
         "feeds_count": len(feeds),
         "feeds": feeds,
+    }
+
+
+@app.get("/web_scrapers", response_model=WebScrapersResponse)
+async def web_scrapers():
+    news = list(itertools.chain(*await scrap_web()))
+
+    return {
+        "news_count": len(news),
+        "news": news,
     }
--- izba_reader/tasks.py
+++ izba_reader/tasks.py
@@ -2,14 +2,25 @@ import asyncio
 
 import httpx
 
-from izba_reader.services import rss
+from izba_reader.services import html, rss
+from izba_reader.services.html import get_browser
 
 
-async def fetch_rss_task():
+async def fetch_rss_feeds():
     async with httpx.AsyncClient() as client:
         return await asyncio.gather(
             *[
                 getattr(rss, rss_service_name)(client)
                 for rss_service_name in rss.__all__
+            ]
+        )
+
+
+async def scrap_web():
+    async with get_browser() as browser:
+        return await asyncio.gather(
+            *[
+                getattr(html, html_service_name)(browser)
+                for html_service_name in html.__all__
             ]
         )

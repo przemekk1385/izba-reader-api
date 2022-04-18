87db17b6d99bd98ed0bd5ffaae15a5de335e0363
Przemysław Kaliś <przemek.kalis@gmail.com>
2022-04-18 09:57:01 +0200

    Add endpoint and some minor fixes

--- izba_reader/constants.py
+++ izba_reader/constants.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-IMAGES_ROOT = BASE_DIR / "images"
-IMAGES_URL = "/images/"
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
--- izba_reader/models.py
+++ izba_reader/models.py
@@ -3,10 +3,6 @@ from datetime import datetime
 from pydantic import BaseModel
 
 
-class Message(BaseModel):
-    detail: str
-
-
 class Feed(BaseModel):
     title: str
     description: str
@@ -17,17 +13,25 @@ class News(Feed):
     date: datetime
 
 
-class BaseResponseModel(BaseModel):
+class BaseServiceResponseModel(BaseModel):
     time: datetime
     count: int
 
 
-class RssFeedsResponse(BaseResponseModel):
+class RssFeedsResponse(BaseServiceResponseModel):
     items: list[Feed]
 
 
-class WebScrapersResponse(BaseResponseModel):
+class WebScrapersResponse(BaseServiceResponseModel):
     items: list[News]
+
+
+class HeadersListResponse(BaseModel):
+    __root__: list[str]
+
+
+class MessageResponse(BaseModel):
+    detail: str
 
 
 class UploadImageResponse(BaseModel):

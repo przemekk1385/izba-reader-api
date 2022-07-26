ca8619130527dfa83b58259e642555e02655d6f3
Przemysław Kaliś <przemek.kalis@gmail.com>
2022-07-26 23:39:01 +0200

    Use payload for generating message

--- izba_reader/services/mail.py
+++ izba_reader/services/mail.py
@@ -1,59 +1,50 @@
-import itertools
+from functools import singledispatch
 
 import rollbar
 from fastapi import Depends, Request
 from fastapi_mail import ConnectionConfig, FastMail, MessageSchema
-from pydantic import EmailStr
+from pydantic import BaseModel
 
-from izba_reader.constants import FEED_URLS, NEWS_URLS
 from izba_reader.decorators import async_report_exceptions
 from izba_reader.dependencies import get_settings
-from izba_reader.services import fetch
+from izba_reader.models import Feed, News, Review
 from izba_reader.settings import Settings
 
 
-async def _make_text_message() -> str:
-    feeds, news = await fetch.rss(FEED_URLS), await fetch.html(NEWS_URLS)
+def _make_text_message(articles: list[Feed | News]) -> str:
+    @singledispatch
+    def format_article(article: BaseModel) -> str:
+        pass
 
-    return "\n\n".join(
-        [
-            "\n\n".join(
-                [
-                    "#rss\n"
-                    f"{item['title']}\n"
-                    f"{item['description']}\n"
-                    f"{item['link']}"
-                    for item in itertools.chain(*feeds.values())
-                ]
-                if feeds
-                else ["no #rss"]
-            ),
-            "\n\n".join(
-                [
-                    "#web\n"
-                    f"{item['date']}\n"
-                    f"{item['title']}\n"
-                    f"{item['description']}\n"
-                    f"{item['link']}"
-                    for item in itertools.chain(*news.values())
-                ]
-                if news
-                else ["no #web"]
-            ),
-        ]
-    )
+    @format_article.register
+    def format_feed(article: Feed) -> str:
+        return (
+            "#feed\n" f"{article.title}\n" f"{article.description}\n" f"{article.url}"
+        )
+
+    @format_article.register
+    def format_news(article: News) -> str:
+        return (
+            "#web\n"
+            f"{article.date}\n"
+            f"{article.title}\n"
+            f"{article.description}\n"
+            f"{article.url}"
+        )
+
+    return "\n\n".join(format_article(article) for article in articles)
 
 
 @async_report_exceptions
 async def send(
-    email: EmailStr,
+    review: Review,
     request: Request,
     settings: Settings = Depends(get_settings),
 ):
-    body = await _make_text_message()
+    body = _make_text_message(review.articles)
 
     message = MessageSchema(
-        subject=settings.mail_subject, recipients=[email], body=body
+        subject=settings.mail_subject, recipients=[review.recipient], body=body
     )
 
     connection_config = ConnectionConfig(
@@ -71,5 +62,8 @@ async def send(
     fm = FastMail(connection_config)
     await fm.send_message(message)
     rollbar.report_message(
-        "Email sent", level="info", extra_data={"email": email}, request=request
+        "Email sent",
+        level="info",
+        extra_data={"email": review.recipient},
+        request=request,
     )
